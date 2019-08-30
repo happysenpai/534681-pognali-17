d71f87732276de02aef1f20da3dc026d6ef75217
happysenpai <32872863+happysenpai@users.noreply.github.com>
2019-08-30 03:46:04 +0300

    fix

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,20 @@
+
+// window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("scroll-menu").style.display = "flex";
+//   } else {
+//     document.getElementById("scroll-menu").style.display = "none";
+//   }
+// }
+
+
 var rateslink     = document.querySelector(".rates__button-show")
-    modalrates    = document.querySelector(".rates-modal");
-    modalclose    = document.querySelector(".rates-modal__button-close");
-    menuclose    = document.querySelector(".menu-modal__list");
-    menulink    = document.querySelector(".intro__menu");
-    modalmenu    = document.querySelector(".menu-modal");
+modalrates    = document.querySelector(".rates-modal");
+modalclose    = document.querySelector(".rates-modal__button-close");
+menuclose    = document.querySelector(".menu-modal__list");
+menulink    = document.querySelector(".intro__menu");
+modalmenu    = document.querySelector(".menu-modal");
 
 rateslink.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -42,3 +53,4 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
